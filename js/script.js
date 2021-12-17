2486281d638adaae3966d0d272f8f20e6603039c
Alessio2021 <alessiobut1911@gmail.com>
2021-12-17 16:01:00 +0100

    milstone 3 + bonus

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 // Stampare all’interno di una lista, un item per ogni todo.
 // Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+// Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
+// Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+
 
 const app = new Vue (
     {
@@ -15,16 +18,21 @@ const app = new Vue (
         },
         methods: {
             add: function() {
-                let newTodo = {
-                    text: this.textNew,
-                    done: false,
+                if (this.textNew != '') {
+                    let newTodo = {
+                        text: this.textNew,
+                        done: false,
+                    }
+                    this.todo.unshift(newTodo)
+                    this.textNew = ''
                 }
-                this.todo.unshift(newTodo)
-                this.textNew = ''
             },
-            checked: function() {
-                this.done = true;
+            checked: function(index) {
+                this.todo[index].done = !this.todo[index].done;
                 console.log('click');
+            },
+            cancel: function(index){
+                this.todo.splice(index, 1)
             }
         }
     

5dae77c0005a3b744655520c35fc0588178b952f
Alessio2021 <alessiobut1911@gmail.com>
2021-12-17 15:15:13 +0100

    check

--- js/script.js
+++ js/script.js
@@ -1,7 +1,32 @@
+// Stampare all’interno di una lista, un item per ogni todo.
+// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+
 const app = new Vue (
     {
         el: '#app',
         data: {
+            textNew: '',
+            todo: [
+                {
+                    text: 'fare bucato',
+                    done: false,
+            }
+        ]
+        },
+        methods: {
+            add: function() {
+                let newTodo = {
+                    text: this.textNew,
+                    done: false,
+                }
+                this.todo.unshift(newTodo)
+                this.textNew = ''
+            },
+            checked: function() {
+                this.done = true;
+                console.log('click');
+            }
+        }
+    
     }
-
-});
+);
